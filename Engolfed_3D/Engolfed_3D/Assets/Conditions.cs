972781e179ee8cf49616fd5e54f3d63f950a0459
Nathan Riopelle <nriopell@andrew.cmu.edu>
2021-09-19 14:57:20 -0400

    Uploaded 3D game

---
+++ Engolfed_3D/Engolfed_3D/Assets/Conditions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conditions : MonoBehaviour
+{
+    public int Points = 0;
+    public OnChangePosition holeScript;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        GrowHole();
+    }
+
+    private void GrowHole()
+    {
+        Points++;
+
+        if(Points % 2 == 0)
+        {
+            StartCoroutine(holeScript.ScaleHole());
+        }
+    }
+}
