ef08793389022987ccf03a5eb59cd7483f6748fb
riopelln <nriopell@andrew.cmu.edu>
2021-10-11 11:21:38 -0400

    Merginging teleport and golf club scenes

--- SteamVRTeleportationTest/Assets/Ball.cs
+++ SteamVRTeleportationTest/Assets/Ball.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     public float force = 3;
+    public float speed = 1.0f;
+    public float drag = 1.0f;
+
+    private Vector3 direction;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +19,17 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        if (speed > 0) {
+            speed -= drag * Time.deltaTime;
+            Debug.Log(GetComponent<Rigidbody>().velocity);
+        }
+        speed -= drag * Time.deltaTime;
+        if (speed < 0)
+        {
+            speed = 0;
+        }
+        transform.position += direction * speed;
+}
 
     private void OnTriggerEnter(Collider other)
     {
@@ -25,7 +39,11 @@ public class Ball : MonoBehaviour
             //dir = -dir.normalized;
             //StartCoroutine(TurnOffClubCollision(collision));
             //GetComponent<Rigidbody>().AddForce(dir * force);
-            GetComponent<Rigidbody>().velocity = other.gameObject.GetComponent<Club>().GetClubVelocity();
+            
+            //GetComponent<Rigidbody>().velocity = other.gameObject.GetComponent<Club>().GetClubVelocity();
+            Vector3 vel_vec = other.gameObject.GetComponent<Club>().GetClubVelocity();
+            speed = vel_vec.magnitude;
+            direction = vel_vec / speed;
             Debug.Log(GetComponent<Rigidbody>().velocity);
         }
     }
